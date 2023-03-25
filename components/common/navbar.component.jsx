54d18c000d27b99cc536bcda507a8d0eba09f97f
koushith <koushith97@gmail.com>
2023-03-26 03:35:19 +0530

    adds mobile nav

--- components/common/navbar.component.jsx
+++ components/common/navbar.component.jsx
@@ -17,13 +17,21 @@ export const NavbarContainer = styled.div`
 
   nav {
     display: flex;
-    height: 80px;
+    height: 60px;
     align-items: center;
     justify-content: space-between;
     margin: 0 auto;
     border-bottom: 1px solid rgba(63, 63, 70, 0.4);
     backdrop-filter: blur(5px);
     max-width: 1200px;
+
+    .logo {
+      transition: all 0.3s;
+      &:hover {
+        color: #2dd4bf;
+        cursor: pointer;
+      }
+    }
   }
   .nav-list {
     list-style: none;
@@ -40,11 +48,7 @@ export const NavbarContainer = styled.div`
         font-size: 1.4rem;
         font-weight: 500;
         color: #e4e4e7;
-        transition-property: color, background-color, border-color,
-          text-decoration-color, fill, stroke, opacity, box-shadow, transform,
-          filter, backdrop-filter, -webkit-backdrop-filter;
-        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-        transition-duration: 0.15s;
+        transition: all 0.3s;
         &:hover {
           color: #2dd4bf;
           cursor: pointer;
@@ -61,6 +65,10 @@ export const NavbarContainer = styled.div`
     .nav-list {
       .mobile-menu {
         display: block;
+        i {
+          font-size: 16px;
+          display: block;
+        }
       }
       li {
         display: none;
@@ -78,6 +86,10 @@ export const NavbarContainer = styled.div`
     .nav-list {
       .mobile-menu {
         display: block;
+        i {
+          font-size: 16px;
+          display: block;
+        }
       }
     }
   }
@@ -107,7 +119,7 @@ export const NavBar = () => {
             <Link href="timeline">Timeline</Link>
           </li>
           <div className="mobile-menu">
-            <i className="fa-duotone fa-bars" onClick={() => setOpen(true)}></i>
+            <i className="fa-solid fa-bars" onClick={() => setOpen(true)}></i>
 
             <MobileMenu open={open} setOpen={setOpen} />
           </div>
--- components/sections/projects/projects.styles.js
+++ components/sections/projects/projects.styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ProjectSectionContainer = styled.section`
   .heading-container {
     max-width: 76rem;
-    margin-top: 200px;
+    margin-top: 4rem;
   }
 
   .projects-container {
