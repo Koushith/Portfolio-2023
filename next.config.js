ba6c28c985575917f3802c0b78da31d97a03dd98
koushith <koushith97@gmail.com>
2023-04-17 22:40:13 +0530

    removes sentry

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,9 +11,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-module.exports = withSentryConfig(
-  module.exports,
-  { silent: true },
-  { hideSourcemaps: true },
-);
