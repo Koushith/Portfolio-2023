a6d1e6e6bfe6865336c15df24bbd1e701eb80cdf
Koushith <koushith97@gmail.com>
2024-07-04 18:45:43 +0530

    removed limit

--- pages/blog.js
+++ pages/blog.js
@@ -4,7 +4,7 @@ const CONTENT_API_KEY = "4a73d91788e9613b62a8310488";
 
 async function getPosts() {
   const res = await fetch(
-    `https://koushith.digitalpress.blog/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt,published_at&limit=all`
+    `https://koushith.digitalpress.blog/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt,published_at`
   ).then((res) => res.json());
 
   const posts = res.posts;
